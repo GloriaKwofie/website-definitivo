324f81cf35d240d4ac33ec0f26c5b0ab4329fc5e
GloriaKwofie <91749390+GloriaKwofie@users.noreply.github.com>
2024-04-06 15:40:23 +0200

    index and app js

--- website-santa-clara-definitivo/frontend/src/App.js
+++ website-santa-clara-definitivo/frontend/src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navi from "./components/Navigation/Navi";
+import Home from "./components/Homepage/Home";
+import Transparencia from "./components/Pages/Transparencia";
+import Footer from "./components/Footer/Footer";
+import Cards from "./components/Cards/Cards";
+import Plataforma from "./components/Pages/Plataforma";
+import Residencia from "./components/Pages/Residencia";
+import Huerto from "./components/Pages/Huerto";
+import Pisos from "./components/Pages/Pisos";
+import Taller from "./components/Pages/Taller";
+import Duchas from "./components/Pages/Duchas";
+import Cosidor from "./components/Pages/Cosidor";
+import Oftamologia from "./components/Pages/Oftamologia";
+import Invulnerables from "./components/Pages/Invulnerables";
+import Corridores from "./components/Pages/Corridores";
+import Josoc from "./components/Pages/Josoc";
+import Equipo from "./components/Pages/Equipo";
+import Fundacion from "./components/Pages/Fundacion";
+import Objectivo from "./components/Pages/Objectivo";
+import Contacto from "./components/Pages/Contact";
+import Voluntarios from "./components/Pages/Voluntarios";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navi />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/transparencia" element={<Transparencia />} />
+      </Routes>
+      <Routes>
+        <Route path="/proyectos" element={<Cards />} />
+      </Routes>
+      <Routes>
+        <Route path="/plataforma" element={<Plataforma />} />
+      </Routes>
+      <Routes>
+        <Route path="/residencia" element={<Residencia />} />
+      </Routes>
+      <Routes>
+        <Route path="/huerto" element={<Huerto />} />
+      </Routes>
+      <Routes>
+        <Route path="/piso" element={<Pisos />} />
+      </Routes>
+      <Routes>
+        <Route path="/taller" element={<Taller />} />
+      </Routes>
+      <Routes>
+        <Route path="/duchas" element={<Duchas />} />
+      </Routes>
+      <Routes>
+        <Route path="/cosidor" element={<Cosidor />} />
+      </Routes>
+      <Routes>
+        <Route path="/oftamologia" element={<Oftamologia />} />
+      </Routes>
+      <Routes>
+        <Route path="/invulnerables" element={<Invulnerables />} />
+      </Routes>
+      <Routes>
+        <Route path="/corridores" element={<Corridores />} />
+      </Routes>
+      <Routes>
+        <Route path="/josoc" element={<Josoc />} />
+      </Routes>
+      <Routes>
+        <Route path="/equipo" element={<Equipo />} />
+      </Routes>
+      <Routes>
+        <Route path="/fundación" element={<Fundacion />} />
+      </Routes>
+      <Routes>
+        <Route path="/objectivos" element={<Objectivo />} />
+      </Routes>
+      <Routes>
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+      <Routes>
+        <Route path="/voluntarios" element={<Voluntarios />} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
--- website-santa-clara-definitivo/frontend/src/index.js
+++ website-santa-clara-definitivo/frontend/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import i18n from "./i18n";
+import { I18nextProvider } from "react-i18next";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
