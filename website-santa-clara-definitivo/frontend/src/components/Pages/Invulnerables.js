6909b0bd5ead2768c6947b690bedab58d8d7fa76
María José Issa <95626214+Majoissa@users.noreply.github.com>
2024-06-09 16:15:49 +0200

    traduccion de pagina invulnerables + solucion de algunos problemas responsivos de css

--- website-santa-clara-definitivo/frontend/src/components/Pages/Invulnerables.js
+++ website-santa-clara-definitivo/frontend/src/components/Pages/Invulnerables.js
@@ -6,8 +6,10 @@ import InvulnerableColl from "../Fcimages/invul-img.jpg";
 import InvulnerableColl2 from "../Fcimages/invul-img2.jpg";
 import InvulnerableColl3 from "../Fcimages/Invul-img3.jpg";
 import InvulnerableColl4 from "../Fcimages/invul-img4.jpg";
+import { useTranslation } from "react-i18next";
 
 function Invulnerables() {
+  const { t } = useTranslation();
   return (
     <div>
       <div className="invulnerables section__padding">
@@ -17,46 +19,29 @@ function Invulnerables() {
               <img src={Invulnerable} class="invulnerables-img" alt="..." />
             </div>
             <p className="invulnerables__text text__grey">
-              "Entre los años 2014 y 2015, los indicadores de pobreza en
-              Cataluña mostraban que el 19% de la población se encontraba en
-              situación de pobreza o riesgo de exclusión social, una cifra que
-              aumentaba hasta el 29,9% en los hogares con menores. Analizando
-              este indicador, se observaba que los hogares con niños eran hasta
-              cuatro veces más pobres que los hogares sin hijos y que el 45,6%
-              de las familias monoparentales estaban en riesgo de exclusión
-              social."
+              {t("inv-description")}
             </p>
           </div>
           <div className="invulnerables-card2-container">
             <div className="invul-card">
               <img src={InvulnerablePic} class="invulnerables-img" alt="..." />
-              <h2>Programa Integral </h2>
+              <h2>{t("program-title")}</h2>
               <p>
-                <strong>#INVULNERABLES</strong> es un programa integral que se
-                enfoca en niños y familias, proporcionando recursos y valores
-                esenciales. El programa busca crear redes cooperativas y ofrece
-                apoyo.
+                <strong>{t("strong-inv")}</strong> {t("p-inv")}
               </p>
             </div>
             <div className="invul-card">
-              <h2>Igualdad de Oportunidades</h2>
-              <p>
-                A través de recursos y esfuerzos colaborativos, el programa
-                tiene como meta asegurar que estos niños tengan acceso a una
-                vida digna y exitosa en el futuro, proporcionándoles las
-                herramientas necesarias para prosperar.
-              </p>
+              <h2>{t("igual")}</h2>
+              <p>{t("p-igual")}</p>
               <img src={InvulnerablePic} class="invulnerables-img" alt="..." />
             </div>
             <div className="invul-card">
-              <h2>Recursos</h2>
-              <h5>QUE OFRECEMOS A LAS FAMÍLIAS DEL PROGRAMA?</h5>
-              <p><strong>Refuerzo escolar, Estraescolares, Terapias, Talleres para padres, Odontologia, Apoyo familiar.</strong></p>
+              <h2>{t("recur")}</h2>
+              <h5>{t("p-recur")}</h5>
               <p>
-                La meta es que estos niños crezcan en un entorno que les
-                garantice no solo igualdad de oportunidades, sino también un
-                futuro prometedor y exitoso.
+                <strong>{t("p-recur-strong")}</strong>
               </p>
+              <p>{t("p-recur-2")}</p>
               <img src={InvulnerablePic} class="invulnerables-img" alt="..." />
             </div>
             <div className="invul-image-collage">
