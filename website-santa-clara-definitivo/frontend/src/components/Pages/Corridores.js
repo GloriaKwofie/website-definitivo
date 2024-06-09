63e282a75431c1be837b264683e0bae4036d31b3
María José Issa <95626214+Majoissa@users.noreply.github.com>
2024-06-09 16:39:48 +0200

    corredores humanitarios

--- website-santa-clara-definitivo/frontend/src/components/Pages/Corridores.js
+++ website-santa-clara-definitivo/frontend/src/components/Pages/Corridores.js
@@ -4,8 +4,10 @@ import Ambulancias from "../Fcimages/ambulancia.jpg";
 import Ucrania from "../Fcimages/ucrania.png";
 import Generadores from "../Fcimages/generadores.jpg";
 import Bizzum from "../Fcimages/bizzum.jpg";
+import { useTranslation } from "react-i18next";
 
 function Corridores() {
+  const { t } = useTranslation();
   return (
     <div>
       <div className="corridores section__padding">
@@ -13,49 +15,36 @@ function Corridores() {
           <div className="corridores__content text__center">
             <div className="corridores-header">
               <h1 className="corridores-section__title text__cap">
-                CORRIDORES HUMANITARIOS
+                {t("corredores")}
               </h1>
             </div>
             <p className="corridores__text text__grey">
-              24 de febrero 2022 Inició la Guerra en Ucrania. Las Necesidades
-              como acogida de personas, ayuda humanitaria y corredores
-              humanitarios era muy importante.
+              {t("corredores-text")}
             </p>
             <div>
               <div className="reto-container">
                 <div class="reto-card">
-                  <h2>RETO HOSPITAL DE CAMPAÑA </h2>
+                  <h2>{t("reto")}</h2>
                   <img src={Bizzum} class="reto-img" alt="..." />
                   <div class="ucrania-part2-card-content"></div>
                 </div>
               </div>
               <div className="ucrania-Part2-container">
                 <div class="ucrania-part2-card">
-                  <h2>Corredores humanitarios</h2>
+                  <h2> {t("corredores")}</h2>
                   <div class="ucrania-part2-card-content">
                     <p>
-                      Para facilitar el traslado y rescate de los heridos,
-                      enviamos:
-                      <strong>
-                        {" "}
-                        90 Ambulancias, Cada Ambulancia salva más de 100 vidas.
-                        61 Generadores, traslado y tratamiento de heridos y
-                        dando acompañamiento.
-                      </strong>
+                      {t("corredores-p")}
+                      <strong> {t("corredores-strong")}</strong>
                     </p>
                   </div>
                 </div>
                 <div class="ucrania-part2-card">
-                  <h2>Acogida de Personas</h2>
+                  <h2> {t("acogida")}</h2>
                   <div class="ucrania-part2-card-content">
                     <p>
-                      Al comenzar la guerra, fuimos a Ucrania para acoger a las
-                      personas que huían para salvar sus vidas.
-                      <strong>
-                        {" "}
-                        Promovimos la insercion laboral, aprendizaje de la
-                        lengua, acompañamiento personalizado,
-                      </strong>
+                      {t("acogida-text")}
+                      <strong> {t("acogida-strong")}</strong>
                     </p>
                   </div>
                 </div>
